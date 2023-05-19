12c0c89126a15428ee4eef813fbb16d201b0edd1
Sayan Sadhukhan <sadhukhansayan1234@gmail.com>
2023-05-19 23:45:04 +0530

    2nd

--- CarRentalManagementSystemAPI/Controllers/DriverController.cs
+++ CarRentalManagementSystemAPI/Controllers/DriverController.cs
@@ -20,5 +20,44 @@ namespace CarRentalManagementSystemAPI.Controllers
         {
             return await _driverSercvice.GetAllDrivers();
         }
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Driver>?> GetDriversById(Guid id)
+        {
+            var result = await _driverSercvice.GetDriverByDriverId(id);
+            if (result == null) { 
+                return NotFound("Driver not exists in the database.");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult<List<Driver>>?> DeleteDriversById(Guid id)
+        {
+            var result = await _driverSercvice.DeleteDriverByDriverId(id);
+            if (result == null)
+            {
+                return NotFound("Driver not exists in the database.");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Driver>>?> AddDriver([FromBody]Driver driver) { 
+            var result = await _driverSercvice.AddDriver(driver);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Driver>>?> UpdateDriverById([FromBody]Driver driver)
+        {
+            var result = await _driverSercvice.UpdateDriverByDriverId(driver);
+            if (result == null)
+            {
+                return NotFound("Driver not exists in the database.");
+            }
+            return Ok(result);
+        }
     }
 }
